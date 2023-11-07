35bf57dca1c7e1d82578049722bb7e5e31ea5433
foresle <foresle@noleron.com>
2023-11-07 03:13:23 +0200

    Updated code to version 1.0.0.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # noto-emoji-animation-parser
 
-This code can parse all [note animated emojis](https://googlefonts.github.io/noto-emoji-animation) from.
+This code can parse all [note animated emojis](https://googlefonts.github.io/noto-emoji-animation).
 
-
-From [website's FAQ](https://googlefonts.github.io/noto-emoji-animation/documentation):
-
-> ### How do I download everything?
-
+> #### How do I download everything?
 > Currently, there is no way to download the whole set — the world is not ready for that many simultaneously dancing emoji.
+> *From [website's FAQ](https://googlefonts.github.io/noto-emoji-animation/documentation)*
 
-foresle: No ;)
+#### foresle: No
+
+![wink](emojis/winking-face.webp)
--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+import asyncio
+import bs4
+import requests
+from slugify import slugify
+from ast import literal_eval
+import aiohttp
+import aiofiles
+
+# Firstly, need to download all possible unicodes for emojis and their descriptions.
+# Emojis have a range 1F600-1F64F.
+response = requests.get('https://www.prosettings.com/emoji-list/')
+soup = bs4.BeautifulSoup(response.text)
+all_emojis_trs = (tr for tr in soup.find_all('tr') if tr.find('th') is None)
+emojis_descriptions = ((literal_eval('0x' + tr.find('a')['href'][1:]), slugify(tr.find_all('td')[3].text)) for tr in
+                       all_emojis_trs)
+
+# Let's make the dict for the best experience in the future.
+# All emojis have saved with the same url, changes only their codes.
+# I will get only a webp format, but you can change link file extensions and file name for gifs, for example.
+all_emojis_links: dict = {}
+for code, description in emojis_descriptions:
+    all_emojis_links[code] = {
+        'url': f'https://fonts.gstatic.com/s/e/notoemoji/latest/{hex(code)[2:]}/512.webp',
+        'file_name': description + '.webp'
+    }
+
+
+async def load_emoji(url: str, file_name: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url) as resp:
+            if resp.ok:
+                await asyncio.sleep(1)
+                async with aiofiles.open(f'emojis/{file_name}', mode='wb') as f:
+                    await f.write(await resp.read())
+                    print(f'{file_name} downloaded!')
+
+
+async def load_emojis():
+    tasks: list = [
+        asyncio.create_task(load_emoji(url=emoji_link['url'], file_name=emoji_link['file_name']))
+        for emoji_link in all_emojis_links.values()
+    ]
+    await asyncio.gather(*tasks)
+
+
 if __name__ == '__main__':
-    pass
+    # Starts async downloading
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(load_emojis())
